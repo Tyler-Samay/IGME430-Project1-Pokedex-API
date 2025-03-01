22470db03e0a883483431f57e4e3be4818f8d035
Ninjasquirrel220 <tyler.samay@hotmail.com>
2025-02-28 21:47:24 -0500

    Working on endpoints + other requirements

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,25 +2,44 @@ const fs = require('fs');
 
 const pokedex = JSON.parse(fs.readFileSync(`${__dirname}/../dataset/pokedex.json`));
 
+const respond = (response, status, data) => {
+    const responseData = JSON.stringify(data);
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(responseData, 'utf8'),
+    });
+    response.write(responseData);
+    response.end();
+};
 
+// Get all Pokemon
+const getAllPokemon = (request, response) => {
+    respond(response, 200, pokedex);
+}
 
 // Get name
 const getName = (request, response) => {
     // Get the name from the query parameters
     const name = request.query.name;
 
+    // If there is no pokemon name
+    if (!name) {
+        respond(response, 400, { error: 'Name parameter required' });
+    }
+
     // Find the pokemon with the name
-    const pokemon = pokedex.find((pokedex) => pokedex.name.toLowerCase() == name.toLowerCase());
+    const pokemon = pokedex.find((pokedex) => pokedex.name === name);
     if (pokemon) {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify(pokemon));
+        respond(response, 200, pokemon);
     }
 
     // If the pokemon is not found, return an error
     else {
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        response.write(JSON.stringify({ error: 'Pokemon not found' }));
+        respond(response, 204, { error: 'Pokemon not found' });
+
     }
+
+
 };
 
 // Get all pokemon of a specific type
@@ -29,28 +48,54 @@ const getType = (request, response) => {
     // Get the type from the query parameters
     const type = request.query.type;
 
-    // Find all pokemon with the type
+    // If no type is selected
+    if (!type) {
+        respond(response, 400, { error: 'Type parameter required' });
+    }
+
     const pokemon = pokedex.filter((pokedex) => pokedex.type.includes(type));
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(pokemon));
-    response.end();
+
+    // If there are no pokemon of that type
+    if (pokemon.length == 0) {
+       respond(response, 204, { error: 'No pokemon of that type found' });
+    }
+
+    // If pokemon of that type are found
+    respond(response, 200, pokemon);
 };
 
 // Get final stage evolution
 const getFinalStageEvolution = (request, response) => {
     const finalStagePokemon = pokedex.filter((pokedex) => !pokedex.next_evolution);
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ finalStagePokemon }));
+    respond(response, 200, finalStagePokemon);
 };
 
 // Get random pokemon
 const getRandomPokemon = (request, response) => {
 
-    // Get a random pokemon from the list
-    const randomPokemon = pokedex[Math.floor(Math.random() * pokedex.length)];
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(randomPokemon));
-    response.end();
+    // How many random pokemon to find
+    const numPokemon = request.query.numPokemon;
+
+    // If no number of pokemon is selected
+    if (!numPokemon || numPokemon < 1 || numPokemon > pokedex.length) {
+        respond(response, 400, { error: 'Number of Pokemon parameter required and must be between 1 and 151' });
+    } 
+
+    // List of random pokemon being displayed
+    const randomPokemonList = [];
+
+    // For the number of random pokemon
+    for (let i = 0; i < numPokemon; i++) {
+        // Get a random pokemon from the list
+        const randomPokemon = pokedex[Math.floor(Math.random() * pokedex.length)];
+        randomPokemonList.push(randomPokemon);
+    }
+
+    respond(response, 200, randomPokemonList);
 };
 
-module.exports = { getName, getType, getFinalStageEvolution, getRandomPokemon };
+const notFound = (request, response) => {
+    respond(response, 404, { error: 'Resource not found' });
+};
+
+module.exports = { getAllPokemon, getName, getType, getFinalStageEvolution, getRandomPokemon, notFound };
--- src/server.js
+++ src/server.js
@@ -7,42 +7,59 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
 
-      // Protocol is http or https
-      const protocol = request.connection.encrypted ? 'https' : 'http';
-      // Parse the URL into 3 parts: protocol, host, and query
-      const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-      // Add the query parameters to the request object (Ex: request.query.valid)
-      request.query = Object.fromEntries(parsedUrl.searchParams);
+    // Protocol is http or https
+    const protocol = request.connection.encrypted ? 'https' : 'http';
 
-  switch (parsedUrl.url) {
-    case '/':
-        htmlHandler.getIndex(request, response);
-        break;
+    // Parse the URL into 3 parts: protocol, host, and query
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    case '/style.css':
-        htmlHandler.getCSS(request, response);
-        break;
+    // Add the query parameters to the request object (Ex: request.query.valid)
+    request.query = Object.fromEntries(parsedUrl.searchParams);
 
-    case '/name':
-        jsonHandler.getName(request, response);
-        break;
+    if (request.method === 'HEAD') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({ message: 'This is a head request' }));
+        response.end();
+        return;
+    }
 
-    case '/type':
-        jsonHandler.getType(request, response);
-        break;
+    switch (parsedUrl.pathname) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
 
-    case '/finalStageEvolution':
-        jsonHandler.getFinalStageEvolution(request, response);
-        break;
+        case '/style.css':
+            htmlHandler.getCSS(request, response);
+            break;
 
-    case '/random':
-        jsonHandler.getRandomPokemon(request, response);
-        break;
+        case '/allPokemon':
+            jsonHandler.getAllPokemon(request, response);
+            break;
 
-    default:
-        htmlHandler.getIndex(request, response);
-        break;
-  }
+        case '/name':
+            jsonHandler.getName(request, response);
+            break;
+
+        case '/type':
+            jsonHandler.getType(request, response);
+            break;
+
+        case '/finalStageEvolution':
+            jsonHandler.getFinalStageEvolution(request, response);
+            break;
+
+        case '/random':
+            jsonHandler.getRandomPokemon(request, response);
+            break;
+
+        case '/notFound':
+            jsonHandler.notFound(request, response);
+            break;
+
+        default:
+            jsonHandler.notFound(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port);
